c02ee81df25687c3e886ac4ee700729ef30f717d
Veter-Alex <Vet-an@yandex.ru>
2024-11-08 12:43:15 +0300

    Changes to be committed: 	modified:   static/js/scipt.js 	modified:   templates/index.html

--- static/js/scipt.js
+++ static/js/scipt.js
@@ -2,3 +2,8 @@ function redirectToUserPage() {
     const userInput = document.getElementById("user-input").value;
     window.location.href = `/user/${userInput}`;
 }
+
+function getfoldertree() {
+    const folderPath = document.getElementById("folder-path").value;
+    window.location.href = `/tree/${folderPath}`;
+}
