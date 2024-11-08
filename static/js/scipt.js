849a8503fefebcf3546582d96115fd06c1800844
Veter-Alex <Vet-an@yandex.ru>
2024-11-08 12:50:05 +0300

    Changes to be committed: 	modified:   static/js/scipt.js 	modified:   templates/index.html

--- static/js/scipt.js
+++ static/js/scipt.js
@@ -3,7 +3,19 @@ function redirectToUserPage() {
     window.location.href = `/user/${userInput}`;
 }
 
-function getfoldertree() {
-    const folderPath = document.getElementById("folder-path").value;
-    window.location.href = `/tree/${folderPath}`;
+function getFolderTree(dirPath) {
+    const folderList = [];
+
+    const dirs = fs.readdirSync(dirPath);
+    dirs.forEach((dir) => {
+        const dirPath = path.join(dirPath, dir);
+        const stats = fs.statSync(dirPath);
+        if (stats.isDirectory()) {
+            folderList.push(dirPath);
+            const subDirs = getFolderTree(dirPath);
+            folderList.push(...subDirs);
+        }
+    });
+
+    return folderList;
 }
